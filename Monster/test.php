0e321b2e90e0f6c88a3a8cd625f51a7bac59452a
Antoine07 <antoine.lucsko@wanadoo.fr>
2016-07-27 16:18:42 +0200

    monster cie

--- Monster/test.php
+++ Monster/test.php
@@ -1,128 +1,28 @@
 <?php
+require_once 'Connect/Connect.php';
+require_once 'Entity/Monster.php';
 
-// configuration
 $database = [
     'dns' => 'mysql:host=localhost;dbname=db_game',
     'username' => 'root',
     'password' => ''
 ];
 
-$options = [
-    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8", // flux en utf8
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,  // mysql erreurs remontées
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ, // données dans des objetds
-];
+$connect = new Connect($database);
 
-$pdo = new PDO($database['dns'], $database['username'], $database['password'], $options);
+$pdo = $connect->getPDO(); // instance de PDO
+//$pdo = null; // couper la connexion
 
-var_dump($pdo);
+$stmt = $pdo->query('SELECT * FROM monsters');
 
-// PDOStatement
-$stmt = $pdo->query('SELECT * FROM `monsters`');
+$Monsters = [];
 
-foreach ($stmt as $monster) {
-    echo "<p>{$monster['name']}</p>";
+var_dump($stmt);
+
+foreach($stmt as $monster)
+{
+    var_dump($monster);
+    // pusher les monsters objet dans le tableau $Monsters
+    $Monsters[] = new Monster($monster);
 }
-
-
-// insertion de données
-
-$sql = sprintf(" INSERT INTO `monsters` (name, life, `force`)
-     VALUES ('%s', %d, %d)
-     ",
-    'dracula',
-    2,
-    2
-);
-
-// $pdo->query($sql);
-
-
-// requête préparé optimiser l'insertion le DQL ...
-
-
-// prepare compilera la requête côté MySQL
-$prepare = $pdo->prepare('INSERT INTO `monsters` (`name`, life, `force`) VALUES (?, ?, ?) ');
-
-var_dump($prepare);
-
-// parameters position place holder, valeur, type attendu par MySQL
-$prepare->bindValue(1, 'kiki', PDO::PARAM_STR);
-$prepare->bindValue(2, 4, PDO::PARAM_INT);
-$prepare->bindValue(3, 4, PDO::PARAM_INT);
-
-$prepare->execute();
-
-// parameters position place holder, valeur, type attendu par MySQL
-$prepare->bindValue(1, 'cyclope', PDO::PARAM_STR);
-$prepare->bindValue(2, 2, PDO::PARAM_INT);
-$prepare->bindValue(3, 2, PDO::PARAM_INT);
-
-$prepare->execute();
-
-
-
-
-
-
-
-
-// injection
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
