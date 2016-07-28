2a04eef89fae234488c486b0fa36ccdbdefe1e45
Antoine07 <antoine.lucsko@wanadoo.fr>
2016-07-28 21:56:19 +0200

    data

---
+++ Heritage/Project/schema.sql
@@ -0,0 +1,16 @@
+# defined schema tables
+
+# data
+
+INSERT INTO `muppets` (name, life, rate) VALUES
+  ('Frozzie', 10, 'NULL'),
+  ('Frozzie', 10, 4),
+  ('Frozzie', 10, 6),
+  ('Frozzie', 10, 7),
+  ('Frozzie', 10, 8),
+  ('Kermit', 7, 5),
+  ('Kermit', 50, 6),
+  ('Kermit', 20, 8),
+  ('Kermit', 11, 'NULL'),
+  ('Kermit', 8, 2),
+  ('Kermit', 8, 3);
